467ca6e4331081717cc5490476d660396c30f2c6
Mustafa <mustafa.nawaz915@gmail.com>
2022-11-06 17:56:29 +0000

    Add button component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,34 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.s(a|c)ss$/,
+      include: path.resolve(__dirname, "../"),
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              auto: true,
+              localIdentName: "[name]__[local]--[hash:base64:5]",
+            },
+          },
+        },
+        "sass-loader",
+      ],
+    });
+    return config;
+  },
+};
